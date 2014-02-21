e4dbe83e5c36a6033107abaa5bb1b15262b82c2c
unknown <bpark92@BATU.csepclab.cs.washington.edu>
2014-02-20 18:11:23 -0800

    initial commit

---
+++ src/tableau/BinarySearch.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package tableau;
+
+/**
+ * @author bpark92
+ *
+ */
+public class BinarySearch<T extends Comparable<T>> {
+
+	/**
+	 * @param args
+	 */
+	public boolean search(T[] array, T element) {
+		
+	
+	}
+}
